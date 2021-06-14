1397ead46f2419091f44e611915a231e0a195f4e
jts581 <jts581@stern.nyu.edu>
2021-06-14 16:31:30 -0400

    Final touches

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -34,35 +34,6 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 # TODO: write some Python code here to produce the desired output
-#Write a program that asks the user to input one or more product identifiers, then looks up the prices for each, then prints 
-# an itemized customer receipt including the total amount owed.
-
-#The program should use one of the provided datastores (see "Data Setup") to represent the store owner's inventory of products 
-# and prices.
-
-#The program should prompt the checkout clerk to input the identifier of each shopping cart item, one at a time.
-
-#When the clerk inputs a product identifier, the program should validate it, displaying a helpful message like "Hey, are you 
-# sure that product identifier is correct? Please try again!" if there are no products matching the given identifier.
-
-#At any time the clerk should be able to indicate there are no more shopping cart items by inputting the word DONE or otherwise 
-# indicating they are done with the process. Before asking for identifiers, the program should provide clear instructions to the user
-# about how to use the "DONE" keyword.
-
-#After the clerk indicates there are no more items, the program should print a custom receipt on the screen. The receipt should
-# include the following components:
-#    A grocery store name of your choice
-#    A grocery store phone number and/or website URL and/or address of choice
-#    The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-#    The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-#    The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-#    The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-#    The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-#    A friendly message thanking the customer and/or encouraging the customer to shop again
-
-#The program should be able to process multiple shopping cart items of the same kind, but need not display any groupings or 
-# aggregations of those items (although it may optionally do so).
-
 
 ### Info capture
 
@@ -117,7 +88,7 @@ print("--------------")
 print("THANKS FOR STOPPING PY :)")
 print("--------------")
 
-## Email receipt - Ran out of time to get to this
+## Email receipt - Believe this is working as I get a 202 code, although SendGrid flagged my account for review so all my outgoing emails are labeled as "processing"
 
 email_option = input("WOULD YOU LIKE AN EMAILED RECEIPT? (Y/N):")
 if email_option == "Y":
