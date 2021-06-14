6ecb3722aa9bbbd7aebfc89a4983885a186bf569
jts581 <jts581@stern.nyu.edu>
2021-06-14 16:27:40 -0400

    Email receipt enabled

--- app/shopping_cart.py
+++ app/shopping_cart.py
@@ -2,16 +2,25 @@ import os
 from typing import Literal
 from dotenv import load_dotenv
 from pandas.core.frame import DataFrame as df
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
 load_dotenv()
+
+# Enable emailed receipts
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDGRID_TEMPLATE_ID = os.getenv("SENDGRID_TEMPLATE_ID", default="OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+RECEIVER_ADDRESS = os.getenv("RECEIVER_ADDRESS", default="OOPS, please set env var called 'RECEIVER_ADDRESS'")
 
 # Loading tax rate env variable  
 tax_rate = float(os.getenv("TAXRATE"))
 
 # Importing pandas and reading csv file  
 from pandas import read_csv
-csv_filepath = (r"C:\Users\jacob\Documents\GitHub\shopping-cart\data\products.csv")
+csv_filepath = (r"C:\Users\jacob\Documents\GitHub\shopping-cart\data\products.csv") #> For some reason, this only started working when I added the 'r' prefix to the file path
 products_df = read_csv(csv_filepath)
-products_dict = df.to_dict(products_df, "records") #> This took me an embarassingly long time to figure out, to get the df in a format that the selected_products function below would loop through, but I got there!
+products_dict = df.to_dict(products_df, "records") #> This took me an embarassingly long time to figure out- to get the df in a format that the selected_products function below would loop through, but I got there!
 
 def to_usd(my_price):
     """
@@ -56,7 +65,7 @@ def to_usd(my_price):
 # aggregations of those items (although it may optionally do so).
 
 
-### INFO CAPTURE
+### Info capture
 
 total_price = 0
 product_ids = []
@@ -73,13 +82,9 @@ while True:
        print("THAT ITEM DOES NOT EXIST, PLEASE ENTER AN ID BETWEEN 1-20 OR DONE")
 
     else:
-        #selected_products = [p for p in products if int(p["id"]) == int(product_id)]
-        #selected_product = selected_products[0]
-        #total_price = total_price + selected_product["price"]
-        #print("Selected product: " + selected_product["name"] + ": " + to_usd(selected_product["price"]))
         product_ids.append(product_id)
 
-### INFO OUTPUT
+### Info output
 
 print("--------------")
 print("JAKE'S PY SHOP")
@@ -113,9 +118,31 @@ print("--------------")
 print("THANKS FOR STOPPING PY :)")
 print("--------------")
 
-## EMAIL RECEIPT - Ran out of time to get to this
+## Email receipt - Ran out of time to get to this
 
-#email_option = input("WOULD YOU LIKE A RECEIPT? (Y/N):")
-#    if email_option == "N":
-#        break
+email_option = input("WOULD YOU LIKE AN EMAILED RECEIPT? (Y/N):")
+if email_option == "Y":
+    # this must match the test data structure
+    template_data = {
+        "total_price_usd": to_usd(grand_total),
+        "human_friendly_timestamp": checkout_time,
+        "products": selected_products
+    } 
+    client = SendGridAPIClient(SENDGRID_API_KEY)
+    print("CLIENT:", type(client))
 
+    message = Mail(from_email=SENDER_ADDRESS, to_emails=RECEIVER_ADDRESS)
+    message.template_id = SENDGRID_TEMPLATE_ID
+    message.dynamic_template_data = template_data
+    print("MESSAGE:", type(message))
+
+    try:
+        response = client.send(message)
+        print("RESPONSE:", type(response))
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+    except Exception as err:
+        print(type(err))
+        print(err)
